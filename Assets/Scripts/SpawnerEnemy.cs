aac335f11ab27f27675c8bb50f9de702795e37aa
Roger Cristian <erogennin@gmail.com>
2023-05-17 23:01:18 -0300

    Coloquei o tank Mark I com animação

--- Assets/Scripts/SpawnerEnemy.cs
+++ Assets/Scripts/SpawnerEnemy.cs
@@ -6,8 +6,11 @@ public class SpawnerEnemy : MonoBehaviour
 {
    // [SerializeField] GameObject enemyToSpawn;
     [SerializeField] GameObject[] enemyToSpawn;
+    [SerializeField] Transform[] localSpawn;
     [SerializeField] int maxEnemies = 5;
+    [SerializeField] float spawnTime = 2f;
     int objectCount = 0;
+    float timeToSpawn = 0f;
 
     void Start()
     {
@@ -15,19 +18,30 @@ public class SpawnerEnemy : MonoBehaviour
     }
 
     void Update()
-    {       
-        SpawEnemies();
+    {   
+       
+           SpawEnemies();
     }
 
-    private void SpawEnemies(){
-        for (int i = 0; i < enemyToSpawn.Length; i++)
-        {
-          if (objectCount < maxEnemies){
+    private void SpawEnemies()
+    {
+        if (Time.time >= timeToSpawn) 
+        { 
+           // Spawna inimigos da lista de forma aleatoria
+            int randomSpawn = Random.Range(0,enemyToSpawn.Length);
 
-                 Instantiate(enemyToSpawn[i], transform.position, transform.rotation);
+            // Define tempo de espera para spawnar um novo inimigo
+            timeToSpawn = Time.time + spawnTime;
+            if (objectCount < maxEnemies){
+                for (int i = 0;i < localSpawn.Length;i++)
+                {
+                 Instantiate(enemyToSpawn[randomSpawn], localSpawn[i].position, localSpawn[i].rotation);
+
+                 objectCount++;             
+                }
             
-                objectCount++;
-          }
-       }
+                }
+       
+        }
     }
 }
