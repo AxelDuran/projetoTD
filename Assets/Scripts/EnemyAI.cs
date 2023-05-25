702a0b470442c3484c718f78abd3e9193bc7bad3
code4all2 <code4allaulaonline@gmail.com>
2023-05-25 18:01:15 -0300

    esbocoEnemy

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    float enemySpeed;
+
+    Vector3 targetPlace;
+    GameObject m_Enemy;
+    NavMeshAgent m_Agent;
+    void Start()
+    {
+        m_Enemy = gameObject;
+        m_Agent = m_Enemy.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        m_Agent.SetDestination(targetPlace);
+    }
+}
